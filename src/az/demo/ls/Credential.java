6e5e249639da387e175307a61c7a6999916d1f2d
NigarMovsumova <nigar.movsumova@pashabank.az>
2019-10-21 17:02:09 +0400

    SD-001: added documentation for singleton class

--- src/az/demo/ls/Credential.java
+++ src/az/demo/ls/Credential.java
@@ -1,13 +1,24 @@
 package az.demo.ls;
 
+/**
+ * Credential- Singleton class
+ */
 public class Credential {
 
     private static Credential credential;
 
+    /**
+     * Constructor is defined as private, because we will
+     * violate the restriction of the class instantiation
+     */
     private Credential() {
         System.out.println("Instance of a class is created");
     }
 
+    /**
+     * Method, that decides whether we need to call private constructor or
+     * return existing instance of a class
+     */
     public static Credential getCredential(){
 
         if(credential==null){
